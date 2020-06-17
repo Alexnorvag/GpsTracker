6e999a88f6a0c806a725b8d18d10dd9c9abf2756
Alexnorvag <norvag@mail.ru>
2020-06-17 19:41:16 +0300

    init

---
+++ src/components/map/map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const Map = () => {
+  return (
+    <View>
+      <Text>This is map component</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Map;
---
+++ src/views/app/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import Map from '../../components/map/map';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello, it's App</Text>
+      <Map />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
